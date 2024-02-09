9abd32bc14c2779406e0a1c6c8367fc5bbe0f14f
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-02-10 01:14:45 +0300

    Routes code along

--- src/index.js
+++ src/index.js
@@ -1,16 +1,89 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, NavLink, Routes } from 'react-router-dom';
 
-function Home() {
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
+
+function NavBar() {
   return (
     <div>
-      <h1>Home!</h1>
+      <NavLink
+        to="/"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        About
+      </NavLink>
+      <NavLink
+        to="/login"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Login
+      </NavLink>
     </div>
   );
 }
 
-function App() {
-  return <Home />;
+function Home() {
+  return <h1>Home!</h1>;
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function About() {
+  return <h1>This is my about component!</h1>;
+}
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
